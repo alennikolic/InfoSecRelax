01a7cfab7bba65d09c68bacdc6afe4ee5c418b4d
Alen Nikolic <57729607+alennikolic@users.noreply.github.com>
2026-08-01 09:40:19 +0200

    Update menu.php

--- src/config/menu.php
+++ src/config/menu.php
@@ -64,6 +64,7 @@ return [
     ['slug' => 'fizicka-bezbednost',       'title' => 'Fizička bezbednost',         'iso_ref' => 'A.7',                       'group' => 'Operacija'],
     ['slug' => 'incidenti',                'title' => 'Upravljanje incidentima',    'iso_ref' => 'A.5.24-5.28',               'group' => 'Operacija'],
     ['slug' => 'kontinuitet-poslovanja',    'title' => 'Kontinuitet poslovanja',     'iso_ref' => 'A.5.29-5.30',               'group' => 'Operacija'],
+    ['slug' => 'uskladjenost',              'title' => 'Usklađenost',                'iso_ref' => 'A.5.31-5.36',               'group' => 'Operacija'],
 
     // --- Ocenjivanje učinka (Poglavlje 9) ---
     ['slug' => 'pokazatelji',              'title' => 'Pokazatelji i merenje',      'iso_ref' => 'Klauzula 9.1',              'group' => 'Ocenjivanje učinka'],
