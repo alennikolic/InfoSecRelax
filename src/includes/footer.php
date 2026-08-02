283efb1ea9ec84cc833afc8b28232f39799c33af
Alen Nikolic <57729607+alennikolic@users.noreply.github.com>
2026-08-02 12:31:24 +0200

    Update footer.php

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -6,6 +6,9 @@
  * funkcije unutra proveravaju da li modal pomoći uopšte postoji na
  * trenutnoj stranici pre nego što ga diraju, pa je bezbedno učitati ga
  * svuda i pre nego što svaka stranica dobije svoj modal pomoći.
+ *
+ * assets/sidebar-scroll.js dovodi aktivnu stavku menija u vidokrug pri
+ * svakom učitavanju stranice - videti komentar u samom fajlu.
  */
 ?>
             </div>
@@ -14,5 +17,6 @@
     </div>
 
     <script src="assets/help-modal.js"></script>
+    <script src="assets/sidebar-scroll.js"></script>
 </body>
 </html>
